94eb4f9d3742562cfc9185f9f4453355a929158f
Laura6012 <laura.turcotte@live.ca>
2020-11-21 11:08:06 -0500

    connexion : utilisation d'un middleware

--- routes/connexionRouter.js
+++ routes/connexionRouter.js
@@ -5,78 +5,65 @@ var router = express.Router();
 var jwt = require('jsonwebtoken');
 var secret = require('../secret');
 
+//Middleware
+var middleware = require('./middleware');
+
 // Mongoose
 var mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://admin:admin123@timeline.9e4sd.mongodb.net/timeline?retryWrites=true&w=majority',
- { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true,useFindAndModify: false });
+  { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false });
 var db = mongoose.connection;
-
-// Vérifier la connection
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", function () {
-  console.log("Connexion réussie dans le router Connexion");
-});
 
 // Les models
 var utilisateurModel = require('../database/Utilisateur');
 var partieModel = require('../database/Partie');
 
+/*===========================================================================*/
 
-/* Permet d'obtenir le formulaire de connexion*/
+/* GET : Obtenir le formulaire de connexion*/
 router.get('/', function (req, res, next) {
   res.render('login_form', { title: 'Timeline Online' });
 });
 
 
-/*  Retour du formulaire complété */
-router.post('/', function (req, res) {
+/*  POST : Retour du formulaire complété */
+router.post('/', middleware.validerCredencesVides, function (req, res) {
 
-  // Validation les crédences de l'utilisateur
-  var courriel = req.body.courriel.trim();
-  var mdp = req.body.mdp.trim();
-
-
+  //Si les crédences ne sont pas vides, on valide la combinaison courriel-mdp
   utilisateurModel.findOne({ courriel: courriel, mdp: mdp }, function (err, utilisateur) {
 
     // Si l'utilisateur existe, on fait le ménage dans ses invitations
     if (!err && utilisateur) {
-      //console.log(retirerInvitationsPerimees(utilisateur));
-      //utilisateur.invitations = retirerInvitationsPerimees(utilisateur);
-
       //Pour résoudre la promesse retournée, il faut utiliser .then
       retirerInvitationsPerimees(utilisateur).then(
+        
+        //Si retirer les invitations périmées a fonctionné
         function (result) {
           utilisateur.invitations = result;
-          console.log("après : " + utilisateur.invitations);
 
           // Mettre à jour l'utilisateur
           utilisateurModel.findOneAndUpdate(utilisateur.id, { invitations: result }).exec(function (err, utilisateurModif) {
-            if (!err)
-              console.log("invitations mises à jour");
+            if (!err) {
+              //Création du token d'authentification
+              var token = jwt.sign({ nom: utilisateur.nom }, secret.secret, { expiresIn: '24h' });
+              res.setHeader('x-access-token', token);
+              res.send({ nom: utilisateur.nom, invitations: utilisateurModif.invitations });
+              //res.redirect('utilisateurs/' + utilisateur.id + '/parties');
+            }
           });
-
-           //Création du token d'authentification
-           var token = jwt.sign({nom:utilisateur.nom}, secret.secret,{expiresIn:'24h'});
-           res.setHeader('x-access-token', token);
-          
-           res.send({nom:utilisateur.nom, invitations:utilisateurModif.invitations});
-
-          //res.redirect('utilisateurs/' + utilisateur.id + '/parties');
         },
+        //Si retirer les invitations n'a PAS fonctionné
         function (error) {
           console.log("err");
         }
       );
-
-
     }
     else {
-      // Sinon on redirige vers la page de connexion avec un message d'erreur
-      res.render('login_form', { title: 'Timeline Online', erreur: 'Courriel et mot de passe obligatoires' });
+      // L'utilisateur n'existe pas et on redirige vers la page de connexion avec un message d'erreur
+      res.render('login_form', { title: 'Timeline Online', erreur: 'Le courriel et le mot de passe ne correspondent pas' });
     }
   });
 });
-
 
 module.exports = router;
 
--- routes/middleware.js
+++ routes/middleware.js
@@ -6,33 +6,53 @@ const secret = require('../secret.js');
 // les pages d'accueil, d'inscription et de connexion).
 
 var checkToken = (req, res, next) => {
-    var token = req.headers['x-access-token'] || req.headers['authorization'];
-  
-    if (token) {
-      if (token.startsWith('Bearer ')) {
+  var token = req.headers['x-access-token'] || req.headers['authorization'];
+
+  if (token) {
+    if (token.startsWith('Bearer ')) {
       // Nettoyage de la chaîne contenant le token
       token = token.slice(7, token.length);
-      }
-        
-      jwt.verify(token, secret.secret, (err, decoded) => {
-        if (err) {
-          return res.json({
-            success: false,
-            message: 'Token is not valid'
-          });
-        } else {
-          req.decoded = decoded;
-          next();
-        }
-      });
-    } else {
-      return res.json({
-        success: false,
-        message: 'Auth token is not supplied'
-      });
     }
-  };
-  
-  module.exports = {
-    checkToken: checkToken
+
+    jwt.verify(token, secret.secret, (err, decoded) => {
+      if (err) {
+        return res.json({
+          success: false,
+          message: 'Token is not valid'
+        });
+      } else {
+        req.decoded = decoded;
+        next();
+      }
+    });
+  } else {
+    return res.json({
+      success: false,
+      message: 'Auth token is not supplied'
+    });
   }
+};
+
+//Est lors du retour du formulaire de connexion
+var validerSiCredencesVides = (req, res, next) => {
+  // Vérifier si les crédences de l'utilisateur sont vides
+  var courriel = req.body.courriel.trim();
+  var mdp = req.body.mdp.trim();
+
+  if (empty(courriel) || empty(mdp)) {
+    return res.send({
+      success: false,
+      message: "Obligatoire pour se connecter"
+    });
+  }
+  else {
+    //On peut passer à l'étape suivante : valider la combinaison courriel-mdp
+    next();
+  }
+};
+
+
+module.exports = {
+  checkToken: checkToken,
+  validerCredencesVides: validerSiCredencesVides
+}
